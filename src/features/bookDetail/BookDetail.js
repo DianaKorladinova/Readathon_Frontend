3a4e6d9510a7e477abd89dfddf0ade079f3d1510
DianaKorladinova <chanchunche@gmail.com>
2023-01-07 10:46:30 +0100

    started working on book detail

--- src/features/bookDetail/BookDetail.js
+++ src/features/bookDetail/BookDetail.js
@@ -1,28 +1,67 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {fetchBookDetail} from "./bookDetailSlice";
-import {useParams} from "react-router-dom";
-import {fetchMonthlyChallenge} from "../monthlyChallenge/monthlyChallengeSlice";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchBookDetail } from './bookDetailSlice'
+import { useParams } from 'react-router-dom'
+import {
+    fetchMonthlyChallenge,
+} from '../monthlyChallenge/monthlyChallengeSlice'
+import { Card, CardContent, CardMedia, Typography } from '@mui/material'
 
-export function BookDetail() {
-    let {bookId} = useParams();
+export function BookDetail () {
+    let { bookId } = useParams()
     bookId = Number(bookId)
     const books = useSelector(state => state.challenge.books)
+    const bookDetail = useSelector(state => state.book.bookDetail)
     const dispatch = useDispatch()
     useEffect(() => {
-        if (books.length===0) {
+        if (books.length === 0) {
             dispatch(fetchMonthlyChallenge())
-            console.log("here")
-        } else {
-            console.log("then here?", books)
-           dispatch(fetchBookDetail(books[bookId]))
+        } else if (!bookDetail) {
+            dispatch(fetchBookDetail(books[bookId]))
         }
     }, [bookId, books, dispatch])
+
+    function renderDetail () {
+        if (bookDetail) {
+            console.log(bookDetail)
+            return (
+                <div className="BookDetail">
+                    <Typography variant="h3" mt={3}>{bookDetail.title}</Typography>,
+                    <br/><br/>
+                    <Card sx={{ maxWidth: 645 }}>
+                        <CardMedia
+                            sx={{ width: 250, height: 350 }}
+                            image="https://ia800606.us.archive.org/view_archive.php?archive=/32/items/olcovers642/olcovers642-L.zip&file=6425240-L.jpg"
+                            title="Book Cover"
+                        />
+                        <CardContent>
+                            <Typography sx={{ fontSize: 20 }}>Book Details</Typography>
+                            <Typography>Author: {books[bookId].author}</Typography>
+                            <Typography>ISBN: {bookDetail.isbn_13}</Typography>
+                            <Typography>Physical Format: {bookDetail.physical_format}</Typography>
+                            <Typography>Publish Date: {bookDetail.publish_date}</Typography>
+                            <Typography>Publishers: {bookDetail.publishers}</Typography>
+                        </CardContent>
+                    </Card>
+                    {/*<Paper elevation={12} style={{ margin: "0px 0px 8px 0px", border: "1px solid black" }}>*/}
+                    {/*    <Typography variant="h3" mt={3}>{bookDetail.title}</Typography>,*/}
+                    {/*    <br/><br/>*/}
+                    {/*    <Typography sx={{ fontSize: 26 }}>Book Details</Typography>*/}
+                    {/*    <Typography sx={{ fontSize: 20 }}>ISBN: {bookDetail.isbn_13}</Typography>*/}
+                    {/*    <Typography sx={{ fontSize: 20 }}>Physical Format: {bookDetail.physical_format}</Typography>*/}
+                    {/*    <Typography sx={{ fontSize: 20 }}>Publish Date: {bookDetail.publish_date}</Typography>*/}
+                    {/*    <Typography sx={{ fontSize: 20 }}>Publishers: {bookDetail.publishers}</Typography>*/}
+                    {/*</Paper>*/}
+                </div>
+            )
+        }
+    }
+
     return (
         <>
-            Ahoj
-            {/*{book ? book.title : "nic"}*/}
+            {renderDetail()}
+            {/*{bookDetail? bookDetail["isbn_10"]:"a"}*/}
         </>
 
-    );
+    )
 }
--- src/features/bookDetail/bookDetailSlice.js
+++ src/features/bookDetail/bookDetailSlice.js
@@ -1,32 +1,34 @@
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
-import {fetchDetail} from "./bookDetailAPI";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { fetchDetail } from './bookDetailAPI'
 
 const initialState = {
-    answers:[]
-};
+    bookDetail: null,
+}
 
 export const fetchBookDetail = createAsyncThunk(
     'bookDetail/fetchBookDetail',
-    async (book) => {
-        const response = await fetchDetail(book);
-        console.log(response)
-        return null
-        // return response.books;
-    }
-);
+    async (book, _) => {
+        return await fetchDetail(book)
+    },
+)
 
 export const bookDetailSlice = createSlice({
     name: 'bookDetail',
     initialState,
     // The `reducers` field lets us define reducers and generate associated actions
     reducers: {
-        reset(state) {
-            state.answers=[]
+        reset (state) {
+            state.answers = []
         },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchBookDetail.fulfilled, (state, action) => {
+            state.bookDetail = action.payload
+        })
 
-    }
-});
+    },
+})
 
-export const {reset} = bookDetailSlice.actions
+export const { reset } = bookDetailSlice.actions
 
-export default bookDetailSlice.reducer;
+export default bookDetailSlice.reducer
