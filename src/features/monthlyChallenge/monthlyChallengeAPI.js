822ceae887faa33841dc8af00e702e1f436dc272
DianaKorladinova <chanchunche@gmail.com>
2023-01-10 19:38:57 +0100

    removed port

--- src/features/monthlyChallenge/monthlyChallengeAPI.js
+++ src/features/monthlyChallenge/monthlyChallengeAPI.js
@@ -1,3 +1,3 @@
 export function fetchChallenge() {
-    return fetch("http://diana.jware-virtual.com:88881/books/monthly-challenge").then(response => response.json())
+    return fetch("http://diana.jware-virtual.com/books/monthly-challenge").then(response => response.json())
 }
